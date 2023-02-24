b8552fbf51d61ab374c40841777e3aae34893f44
Tushar <tusharsinghynr@gmail.com>
2023-02-24 21:30:09 +0530

    widgets fix

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Text from './Text';
-import Touchable from './Touchable';
-
-export { Text, Touchable };
--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,4 +1,6 @@
 import Button from './Button';
 import Input from './Input';
+import Text from './Text';
+import Touchable from './Touchable';
 
-export { Button, Input };
+export { Button, Input, Text, Touchable };
